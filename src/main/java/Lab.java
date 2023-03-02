03a012fcc6fc16cf79f910d799edf0ac59c39f52
gcruzBwise c <gcruz28@wgu.edu>
2023-03-02 19:31:34 +0000

    Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,9 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int a = 0, b = 10;
+        int c = b/a;
+        System.out.println("Value of c is : "+ c);
+       //  ArithmeticExeption is occurred due to denominator value is zero.
     }
 }
